164581f90d66b453d257e145333f0c952026cf92
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-03-18 16:08:07 +0200

    refactor place of tests

---
+++ tests/MQ/Tests/Util/Timer/WatchdogTimerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MQ\Tests\Unit\Util\Timer;
+
+/**
+ *
+ * @package MQ
+ * @subpackage Test
+ *
+ * @author Maksim Kotlyar <mkotlar@ukr.net>
+ *
+ */
+class WatchdogTimerTest extends \PHPUnit_Framework_TestCase
+{
+  public function testFoo()
+  {
+
+  }
+}
